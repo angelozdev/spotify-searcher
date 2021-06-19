0f10a1a80ff241f9780a8c24ae4dd2356a9778c7
Angelo Zambrano <angelo@ubidots>
2021-06-19 15:53:52 -0500

    style: styled home

--- src/views/Home/home.styles.ts
+++ src/views/Home/home.styles.ts
@@ -1,3 +1,66 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 export const Container = styled.section``;
+
+export const Layout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  height: 100vh;
+  max-height: 900px;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+`;
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  line-height: 3rem;
+`;
+
+export const Subtitle = styled.h2`
+  font-weight: 500;
+  font-size: 1.5rem;
+`;
+
+export const Button = styled.button`
+  padding: 1rem;
+  border-radius: 2rem;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  background-color: #1ed760;
+  transition: 300ms all ease;
+
+  &:hover {
+    transform: scale(1.01);
+  }
+
+  &:active {
+    transform: scale(0.99);
+  }
+`;
+
+export const ButtonText = styled.span`
+  padding: 0 2rem;
+  text-transform: uppercase;
+  color: white;
+`;
+
+export const ImageContainer = styled.div`
+  height: 100%;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const Figure = styled.figure`
+  margin: 0;
+  width: 100%;
+  height: 100%;
+`;
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 
 import { Wrapper } from "components";
-import { Container } from "./home.styles";
+import {
+  Button,
+  ButtonText,
+  Container,
+  Content,
+  Image,
+  ImageContainer,
+  Layout,
+  Subtitle,
+  Title,
+  Figure,
+} from "./home.styles";
 
 function Home() {
   return (
     <Container>
-      <Wrapper>Hola mundo</Wrapper>
+      <Wrapper>
+        <Layout>
+          <Content>
+            <Title>Bienvenido de nuevo.</Title>
+            <Subtitle>Identifícate para encontrar tu música favorita.</Subtitle>
+            <Button>
+              <ButtonText>Iniciar sesión</ButtonText>
+            </Button>
+          </Content>
+
+          <ImageContainer>
+            <Figure>
+              <Image src="https://images.unsplash.com/photo-1567014033256-0cfcd4f84c15?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" />
+            </Figure>
+          </ImageContainer>
+        </Layout>
+      </Wrapper>
     </Container>
   );
 }
