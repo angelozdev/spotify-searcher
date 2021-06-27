3dbe2733788df38e375355176e90677ad111ff4b
Angelo Zambrano <angelo@ubidots>
2021-06-26 20:14:12 -0500

    feat: add home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import * as constants from "consts";
 import { PrivateRoute } from "components";
-import { Login } from "views";
+import { Login, Home } from "views";
 
 function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path={constants.Routes.LOGIN} component={Login} />
-        <PrivateRoute
-          exact
-          path={constants.Routes.SEARCH}
-          component={() => <p>Hola mundo!</p>}
-        />
+        <PrivateRoute exact path={constants.Routes.SEARCH} component={Home} />
       </Switch>
     </Router>
   );
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from "./home.styles";
+
+function Home() {
+  return <Container>Hola HOME</Container>;
+}
+
+export default Home;
